a9295c2d832aba947357d2bcbc4531d0d56ab655
Isabella Abatgis <abatgis@gmail.com>
2017-06-26 20:44:41 -0700

    hid API key

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "ed5a2a8828a5bd57bd4421a9e47fe868";
+
+},{}],2:[function(require,module,exports){
 function Calculator(skinName) {
   this.skin = skinName;
 }
@@ -21,7 +24,7 @@ Calculator.prototype.pingPong = function(goal) {
 
 exports.calculatorModule = Calculator;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 
 $(document).ready(function() {
@@ -49,16 +52,22 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = "ed5a2a8828a5bd57bd4421a9e47fe868";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response){
+        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+     })
+
+     .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
   });
 });
 
-},{"./../js/pingpong.js":1}]},{},[2]);
+},{"./../.env":1,"./../js/pingpong.js":2}]},{},[3]);
--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,11 +1,17 @@
-var apiKey = "ed5a2a8828a5bd57bd4421a9e47fe868";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response){
+        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+     })
+
+     .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
   });
 });
