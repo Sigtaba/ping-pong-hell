cff303caa8e7f60998efee6ac985bf52da10bf96
Isabella Abatgis <abatgis@gmail.com>
2017-06-27 18:21:27 -0700

    add css and sass via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var buildProduction = utilities.env.production;
 var jshint = require('gulp-jshint');
 
@@ -40,6 +42,7 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(["sass/*.sass"], ['cssBuild']);
 });
 
 gulp.task('htmlBuild', function() {
@@ -95,10 +98,20 @@ gulp.task('build', ['clean'], function(){
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
 });
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src(['sass/*.sass'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
